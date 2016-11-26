894d6c7835f8d3ecaf0cded94094b741d694c44f
App Academy Student <student@appacademy.io>
2016-11-25 17:06:23 -0800

    add w2d5 LRUCache

--- w2d5/LRUCache_aa/lib/p02_hashing.rb
+++ w2d5/LRUCache_aa/lib/p02_hashing.rb
@@ -4,11 +4,24 @@ end
 
 class Array
   def hash
+    sum = 0.hash
+    self.each do |el|
+      sum += el.hash
+      sum = sum.hash
+    end
+    (sum.hash + self.length.hash).hash
   end
 end
 
 class String
   def hash
+    str_arr = self.split("")
+    sum = 0
+    str_arr.each_with_index do |el, i|
+      sum += (el.ord + i).hash
+      sum = sum.hash
+    end
+    sum.hash
   end
 end
 
@@ -16,6 +29,15 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sum = self.length.hash
+    sorted_keys = self.keys.sort
+    sorted_keys.each do |key|
+      sum = sum.hash + self[key].ord.hash
+      sum += sum.hash
+    end
+
+    self.length.times { sum = sum.hash }
+
+    sum
   end
 end
--- w2d5/LRUCache_aa/lib/p03_hash_set.rb
+++ w2d5/LRUCache_aa/lib/p03_hash_set.rb
@@ -9,18 +9,26 @@ class HashSet
   end
 
   def insert(key)
+    resize! if @count >= num_buckets
+    self[key] << key
+    @count += 1
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    self[key].delete(key)
+    @count -= 1
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    spot = num.hash % num_buckets
+    @store[spot]
   end
 
   def num_buckets
@@ -28,5 +36,14 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(@store.size * 2) { [] }
+    @store.each do |bucket|
+      bucket.each do |item|
+        spot = item % new_store.size
+        new_store[spot].push(item)
+      end
+    end
+
+    @store = new_store
   end
 end
